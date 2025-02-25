3ce4a60428f3d7e09169e63dd5ee6c6728868fbe
srj0407 <srj0407@gmail.com>
2025-02-24 19:41:39 -0800

    everything lmao

--- api/index.js
+++ api/index.js
@@ -3,4 +3,5 @@
 module.exports = (req, res) => {
     const { name = 'World' } = req.query;
     res.status(200).send(`Hello ${name}!`);
+    res.status(200).json({ message: "Test route is working!" });
     };
